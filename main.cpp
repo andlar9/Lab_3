097eff56c5e4e821da264215c33761af41510d15
Larin <Andrey.Larin.A@yandex.ru>
2014-10-24 10:19:06 +0400

    1.5

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 int main()
 {
     int n,m;
-    cout<<"Enter first number";
+    cout<<"Enter first number ";
     cin>>n;
-    cout<<"\n"<<Enter second number";
+    cout<<"\n"<<"Enter second number ";
     cin>>m;
+    cout<<"\n"<<"sum numbers "<< n+m < "\n" ;
 }
